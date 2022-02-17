c73c94ac90ceb018e2dd7f99555ca59a7a09e716
unk0x <wender.tsx@gmail.com>
2022-02-17 01:03:44 -0300

    return packet server stringify

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { generate } from "./tools";
 
 const packetServer = generate("Server");
 
-const filter = packetServer?.filter((packet) => packet.opcode === "E7") // SendMacroList [C4]
+// const filter = packetServer?.filter((packet) => packet.opcode === "E7") // SendMacroList [C4]
 
-console.log(JSON.stringify(filter, null, 2));
+
+console.log(JSON.stringify(packetServer, null, 2));
